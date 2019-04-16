b5790c8e367bf413c6cf84b6b060dfdea91b9411
gwu358 <gwu358@gmail.com>
2019-04-16 18:11:22 -0400

    pull sth

---
+++ client/components/Checkout.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import axios from 'axios'
+import SingleBook from './SingleBook'
+
+/**
+ * COMPONENT
+ */
+
+class Checkout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+}
---
+++ client/store/cart.js
@@ -0,0 +1,66 @@
+// export const LOAD_CART = 'LOAD_CART';
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+const SUBMIT_ORDER = 'SUBMIT_ORDER';
+
+initialState = {
+    books: []
+}
+
+//action creators
+// export const loadCart = function(books){
+//     return {
+//         type: LOAD_CART,
+//         books
+//     }
+// }
+
+/**
+ * ACTION CREATORS
+ */
+const addBook = function (book) {
+    return {
+        type: ADD_BOOK,
+        book
+    }
+}
+
+const removeBook = function (book) {
+    return {
+        type: REMOVE_BOOK,
+        book
+    }
+}
+
+const submitOrder = function (books) {
+    return {
+        type: SUBMIT_ORDER,
+        book
+    }
+}
+
+const postOrder = function (books){
+    return function (dispatch){
+        dispatch(submitOrder(books));
+    }
+}
+
+/**
+ * REDUCER
+ */
+export default function cartReducer(state = initialState, action) {
+    const newState = { ...state };
+    switch (action.type) {
+        case ADD_BOOK:
+            newState.books = [...newState.books, action.book];
+            break;
+
+        case REMOVE_BOOK:
+            newState.books = newState.books.filter(book => book.id !== action.book.id);
+        break;
+      
+        default:
+            return newState;
+    }
+    return newState;
+}
