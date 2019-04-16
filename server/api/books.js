a76d2c45e5858a29b1b64a12d5039128a083a995
Grace Lee <igracelee1@gmail.com>
2019-04-16 12:14:58 -0400

    book routes seed attempt

---
+++ server/api/books.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const {Book} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+    try {
+      const books = await Book.findAll()
+      res.json(books)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+router.get('/:id', async (req, res, next) => {
+    try {
+      const singleBook = await Book.findById(req.params.id)
+      res.json(singleBook)
+    } catch (err) {
+      next(err)
+    }
+  })
