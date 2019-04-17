280de96591750fe4e0547f24ce6e64bf9f9673bf
Juan Marges <juanmarges97@gmail.com>
2019-04-17 17:17:45 -0400

    created cart route for cart sessions

--- server/api/cart.js
+++ server/api/cart.js
@@ -3,25 +3,35 @@ const {Order, Book} = require('../db/models')
 const {Op} = require('sequelize');
 module.exports = router
 
-
-router.put('/addToCart', async (req, res, next) => {
+router.get('/', (req, res, next) => {
   try {
-    req.session.cart.books.push(req.body);
-    console.log('added!', req.session.cart)
-    res.sendStatus(200)
+    res.send(req.session.cart.books);
   } catch (err) {
     next(err)
   }
 })
 
-router.delete('/removeFromCart/:bookId', async (req, res, next) => {
+router.post('/', (req, res, next) => {
+  try {
+    console.log('session',req.session.cart)
+    req.session.cart.books.push(req.body.book);
+    console.log('added!', req.session.cart)
+    
+    res.status(200).send(req.session.cart)
+
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:bookId', (req, res, next) => {
     try {
-        req.session.cart.books = req.session.cart.books.filter(book => {
-            book.id !== req.params.bookId
-        })
-        console.log('delete!', req.session.cart)
-        res.sendStatus(200)
-    } catch (err) {
+        const bookIdx = req.session.cart.books.findIndex(book => book.id === Number(req.params.bookId));
+        console.log('bookIdx', bookIdx);
+        req.session.cart.books.splice(bookIdx,1);
+        console.log('deleted', req.session.cart);
+        res.status(204).send(req.session.cart);
+      } catch (err) {
       next(err)
     }
   })
