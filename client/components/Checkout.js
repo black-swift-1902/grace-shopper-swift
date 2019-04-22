b062ee2b7b50e2f2fecda2c4adcd44232e695dcc
Juan Marges <juanmarges97@gmail.com>
2019-04-22 10:45:51 -0400

    added some styling

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {
-  getCartFromSession,
-  removeBookThunk,
-  submitOrder
-} from '../store/cart'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getCartFromSession, removeBookThunk, submitOrder} from '../store/cart'
 /**
  * COMPONENT
  */
@@ -19,39 +15,52 @@ class Checkout extends Component {
   }
 
   render() {
-    let { books } = this.props;
-    return (
-      <div>
+    let {books} = this.props
+    let totalPrice = books.reduce(priceSum, 0)
+    return <div>
+        <h1 className="is-size-2 has-text-centered">Checkout</h1>
         {books.map((book, index) => {
-          return (
-            <div key={`book-${book.id}`}>
-              <h2>{book.title}</h2>
-              <button
-                onClick={() => {
-                  this.props.removeBookThunk(index);
-                }}
-              >
-                delete
-              </button>
-              <img src={book.imgUrl} />
-              <h4>{book.price}</h4>
-              <h4>{book.order_log.quantity}</h4>
+          return <div key={`book-${book.id}`} className="columns">
+              <div className="column">
+                <h2 className="is-size-3">Item #{index + 1}</h2>
+                <h2 className="is-size-3">{book.title}</h2>
+                <figure className="image is-128x128">
+                  <img src={book.imgUrl} />
+                </figure>
+              </div>
+              <div className="column">
+                <hr />
+                <h4>Single Price: $ {book.price}</h4>
+                <h4>Quantity: {book.order_log.quantity}</h4>
+                <h4>
+                  Total Price: $
+                  {(book.price * book.order_log.quantity).toFixed(2)}
+                </h4>
+                <a className="button is-warning" onClick={() => {
+                    this.props.removeBookThunk(index)
+                  }}>
+                  <i className="fas fa-trash-alt" />
+                  Delete
+                </a>
+              </div>
             </div>
-          )
         })}
-        {
-          books.length ? <button
-            onClick={() =>
-              this.props.submitOrder()
-            }
-          >
+        <hr />
+        <div>
+          <h4 className="">Subtotal: $ {totalPrice.toFixed(2)}</h4>
+          <h4>Tax: $ {(totalPrice * 0.08875).toFixed(2)}</h4>
+          <h4>Total: $ {(totalPrice * 1.08875).toFixed(2)}</h4>
+        </div>
+        {books.length ? <a onClick={() => this.props.submitOrder()} className="button is-primary">
             Submit Order
-        </button> : undefined
-        }
-
+          </a> : undefined}
       </div>
-    )
   }
+}
+
+const priceSum = function(total, book) {
+  total += book.price * book.order_log.quantity
+  return total
 }
 
 const mapState = state => {
